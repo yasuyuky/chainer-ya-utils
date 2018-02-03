c269243ebfee4885d6242f82a18459ea74aed24a
yasuyuky <yasuyuki.ymd@gmail.com>
2018-02-03 20:44:22 +0900

    Add MemoryUsage extension

--- chainer/ya/utils/__init__.py
+++ chainer/ya/utils/__init__.py
@@ -3,3 +3,4 @@ from .backup import SourceBackup, ArgumentBackup # noqa
 from .request import FinalRequest # noqa
 from .slack import SlackPost # noqa
 from .sample_paring import SamplePairingDataset # noqa
+from .check_memory_usage import MemoryUsage # noqa
---
+++ chainer/ya/utils/check_memory_usage.py
@@ -0,0 +1,21 @@
+from chainer.training import extension
+import chainer
+
+
+class MemoryUsage(extension.Extension):
+
+    def __init__(self, trigger=(1, "epoch")):
+        self.total = []
+        self.trigger = trigger
+
+    def initialize(self, _):
+        if chainer.cuda.available:
+            chainer.cuda.memory_pool.free_all_blocks()
+
+    def __call__(self, _):
+        if chainer.cuda.available:
+            total = chainer.cuda.memory_pool.total_bytes()/(1024*1024)
+            self.total.append(total)
+
+    def finalize(self):
+        pass
